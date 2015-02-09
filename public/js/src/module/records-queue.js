7af31f943b766063f26f1702cc7fc77a6a68d9dc
Martijn van de Rijdt <martijn@enketo.org>
2015-02-09 14:44:59 -0700

    upload records sequentially

--- public/js/src/module/records-queue.js
+++ public/js/src/module/records-queue.js
@@ -39,25 +39,54 @@ define( [ 'store', 'connection', 'q', 'settings', 'translator' ], function( stor
             .then( _updateRecordList );
     }
 
+    /**
+     * Obtains a record
+     *
+     * @param  {string} instanceId [description]
+     * @return {Promise}            [description]
+     */
     function get( instanceId ) {
         return store.record.get( instanceId );
     }
 
+    /**
+     * Stores a new record
+     *
+     * @param {*} record [description]
+     * @return {Promise}
+     */
     function set( record ) {
         return store.record.set( record )
             .then( _updateRecordList );
     }
 
+    /**
+     * Updates an existing record
+     *
+     * @param  {record} record [description]
+     * @return {Promise}        [description]
+     */
     function update( record ) {
         return store.record.update( record )
             .then( _updateRecordList );
     }
 
-    function remove( record ) {
+    /**
+     * Removes a record
+     * @param  {string} intanceId [description]
+     * @return {Promise}        [description]
+     */
+    function remove( instanceId ) {
         return store.record.remove( record )
             .then( _updateRecordList );
     }
 
+    /**
+     * Gets the countervalue of a new record (guaranteed to be unique)
+     *
+     * @param  {string} enketoId [description]
+     * @return {Promise}          [description]
+     */
     function getCounterValue( enketoId ) {
         return store.property.getSurveyStats( enketoId )
             .then( function( stats ) {
@@ -65,21 +94,36 @@ define( [ 'store', 'connection', 'q', 'settings', 'translator' ], function( stor
             } );
     }
 
+    /**
+     * Marks a record as active (opened)
+     *
+     * @param {string} instanceId [description]
+     */
     function setActive( instanceId ) {
         $( '.record-list__records' )
             .find( '.active' ).removeClass( 'active' )
             .addBack().find( '[data-id="' + instanceId + '"]' ).addClass( 'active' );
     }
 
+    /**
+     * Sets the interval to upload queued records
+     */
     function _setUploadIntervals() {
+        // one quick upload attempt soon after page load
         setTimeout( function() {
             uploadQueue();
         }, 30 * 1000 );
+        // interval to check upload queued records
         setInterval( function() {
             uploadQueue();
         }, 5 * 60 * 1000 );
     }
 
+    /**
+     * Uploads all final records in the queue
+     *
+     * @return {Promise} [description]
+     */
     function uploadQueue() {
         var successes = [],
             fails = [];
@@ -93,40 +137,49 @@ define( [ 'store', 'connection', 'q', 'settings', 'translator' ], function( stor
 
             store.record.getAll( settings.enketoId, true )
                 .then( function( records ) {
-                    console.debug( 'records length', records.length );
-                    // TODO: should these be done sequentially? Would look nicer in sidebar...
-                    records.forEach( function( record ) {
-                        // get the whole record including files
-                        store.record.get( record.instanceId )
-                            .then( function( record ) {
-                                connection.uploadRecord( record );
-                                uploadProgress.update( record.instanceId, 'ongoing', '', successes.length + fails.length, records.length );
-                            } )
-                            .then( function( result ) {
-                                successes.push( record.name );
-                                // TODO: remove from store
-                                uploadProgress.update( record.instanceId, 'success' );
-                            } )
-                            .catch( function( result ) {
-                                fails.push( record.name );
-                                uploadProgress.update( record.instanceId, 'error', gui.getErrorResponseMsg( result.status ) );
-                            } )
-                            .then( function() {
-                                if ( successes.length + fails.length === records.length ) {
-                                    uploadOngoing = false;
-                                    if ( successes.length > 0 ) {}
-                                    $( document ).trigger( 'queuesubmissionsuccess', successes );
-                                    // TODO update record list
-                                }
-                            } );
-                    } );
+                    // Perform record uploads sequentially for nicer feedback and to avoid issues when connections are very poor
+                    return records.reduce( function( prevPromise, record ) {
+                        return prevPromise.then( function() {
+                            // get the whole record including files
+                            return store.record.get( record.instanceId )
+                                .then( function( record ) {
+                                    uploadProgress.update( record.instanceId, 'ongoing' );
+                                    return connection.uploadRecord( record );
+                                } )
+                                .then( function( result ) {
+                                    successes.push( record.name );
+                                    // TODO: remove from store
+                                    uploadProgress.update( record.instanceId, 'success', '', successes.length + fails.length, records.length );
+                                } )
+                                .catch( function( result ) {
+                                    fails.push( record.name );
+                                    uploadProgress.update( record.instanceId, 'error', gui.getErrorResponseMsg( result.status ), successes.length + fails.length, records.length );
+                                } )
+                                .then( function() {
+                                    if ( successes.length + fails.length === records.length ) {
+                                        uploadOngoing = false;
+                                        if ( successes.length > 0 ) {
+
+                                        }
+                                        $( document ).trigger( 'queuesubmissionsuccess', successes );
+                                        // TODO update record list
+
+                                    }
+                                } );
+                        } );
+                    }, new Q( null ) );
                 } );
         }
     }
 
+    /**
+     * Shows upload progress and record-specific feedback
+     *
+     * @type {Object}
+     */
     uploadProgress = {
         _getLi: function( instanceId ) {
-            return $( '.record-list__records__record' ).find( '[data-id="' + instanceId + '"]' );
+            return $( '.record-list__records__record[data-id="' + instanceId + '"]' );
         },
         _reset: function( instanceId ) {
             var $allLis = $( '.record-list_records' ).find( 'li' );
@@ -159,21 +212,21 @@ define( [ 'store', 'connection', 'q', 'settings', 'translator' ], function( stor
         },
         update: function( instanceId, status, msg, index, total ) {
             var $result,
-                $lis = this._getLi( instanceId ),
+                $li = this._getLi( instanceId ),
                 displayMsg = this._getMsg( status, msg );
 
-            console.debug( 'updating progress', instanceId, status, msg, index, total );
+            console.debug( 'updating progress', $li, instanceId, status, msg, index, total );
             this._reset( instanceId );
 
             //add display messages (always showing end status)
             if ( displayMsg ) {
-                $result = $( '<li data-id="' + instanceId + '" class="' + status + '">' + displayMsg + '</li>' ).insertAfter( $lis.last() );
+                $result = $( '<li data-id="' + instanceId + '" class="' + status + '">' + displayMsg + '</li>' ).insertAfter( $li );
                 window.setTimeout( function() {
                     $result.hide( 500 );
                 }, 3000 );
             }
 
-            this._updateClass( $lis.first(), status );
+            this._updateClass( $li, status );
             if ( index && total ) {
                 this._updateProgressBar( index, total );
             }
@@ -186,6 +239,11 @@ define( [ 'store', 'connection', 'q', 'settings', 'translator' ], function( stor
         }
     };
 
+    /**
+     * Updates the record list in the UI
+     *
+     * @return {Promise} [description]
+     */
     function _updateRecordList() {
         var $newRecordList, $li,
             deferred = Q.defer();
@@ -227,7 +285,7 @@ define( [ 'store', 'connection', 'q', 'settings', 'translator' ], function( stor
     }
 
     /**
-     * Completely flush the form cache (not the data storage)
+     * Completely flush the form cache (not the record storage)
      *
      * @return {Promise} [description]
      */
