ff06bdffde9bd9833d1e7a9ec523de565b283e4e
Martijn van de Rijdt <martijn@enketo.org>
2015-02-10 11:20:34 -0700

    added offline views to API

--- test/server/api-controller.spec.js
+++ test/server/api-controller.spec.js
@@ -79,13 +79,15 @@ describe( 'api', function() {
             instance = typeof test.instance !== 'undefined' ? test.instance : '<data></data>',
             instanceId = typeof test.instanceId !== 'undefined' ? test.instanceId : 'someUUID:' + Math.random(),
             endpoint = test.endpoint,
-            resProp = ( test.res && test.res.property ) ? test.res.property : 'url';
+            resProp = ( test.res && test.res.property ) ? test.res.property : 'url',
+            offlineEnabled = !!test.offline;
 
         it( test.method.toUpperCase() + ' /api/v' + version + endpoint + ' with ' + authDesc + ' authentication and ' + server + ', ' +
             id + ', ' + ret + ', ' + instance + ', ' + instanceId + ', ' + test.theme +
             ', parentWindowOrigin: ' + test.parentWindowOrigin + ', defaults: ' + JSON.stringify( test.defaults ) +
             ' responds with ' + test.status,
             function( done ) {
+                app.set( 'offline enabled', offlineEnabled );
                 request( app )[ test.method ]( '/api/v' + version + endpoint )
                     .set( auth )
                     .send( {
@@ -110,220 +112,313 @@ describe( 'api', function() {
     describe( 'v1', function() {
         var version = 1;
 
-        v1Survey = [
-            //valid token
-            {
-                method: 'get',
-                auth: true,
-                status: 200
-            }, {
-                method: 'post',
-                auth: true,
-                status: 200
-            }, {
-                method: 'put',
-                auth: true,
-                status: 405
-            }, {
-                method: 'delete',
-                auth: true,
-                status: 204
-            },
-            //invalid token
-            {
-                method: 'get',
-                auth: false,
-                status: 401
-            }, {
-                method: 'post',
-                auth: false,
-                status: 401
-            }, {
-                method: 'put',
-                auth: false,
-                status: 401
-            }, {
-                method: 'delete',
-                auth: false,
-                status: 401
-            },
-            //missing token
-            {
-                method: 'get',
-                auth: null,
-                status: 401
-            }, {
-                method: 'post',
-                auth: null,
-                status: 401
-            }, {
-                method: 'put',
-                auth: null,
-                status: 401
-            }, {
-                method: 'delete',
-                auth: null,
-                status: 401
-            },
-            //non-existing account
-            {
-                method: 'get',
-                auth: true,
-                status: 403,
-                server: invalidServer
-            }, {
-                method: 'post',
-                auth: true,
-                status: 403,
-                server: invalidServer
-            }, {
-                method: 'put',
-                auth: true,
-                status: 403,
-                server: invalidServer
-            }, {
-                method: 'delete',
-                auth: true,
-                status: 403,
-                server: invalidServer
-            },
-            //server_url not provided or empty
-            {
-                method: 'get',
-                auth: true,
-                status: 400,
-                server: ''
-            }, {
-                method: 'post',
-                auth: true,
-                status: 400,
-                server: ''
-            }, {
-                method: 'put',
-                auth: true,
-                status: 400,
-                server: ''
-            }, {
-                method: 'delete',
-                auth: true,
-                status: 400,
-                server: ''
-            }
-        ];
+        describe( '/survey endpoint responses', function() {
+            v1Survey = [
+                //valid token
+                {
+                    method: 'get',
+                    auth: true,
+                    status: 200
+                }, {
+                    method: 'post',
+                    auth: true,
+                    status: 200
+                }, {
+                    method: 'put',
+                    auth: true,
+                    status: 405
+                }, {
+                    method: 'delete',
+                    auth: true,
+                    status: 204
+                },
+                //invalid token
+                {
+                    method: 'get',
+                    auth: false,
+                    status: 401
+                }, {
+                    method: 'post',
+                    auth: false,
+                    status: 401
+                }, {
+                    method: 'put',
+                    auth: false,
+                    status: 401
+                }, {
+                    method: 'delete',
+                    auth: false,
+                    status: 401
+                },
+                //missing token
+                {
+                    method: 'get',
+                    auth: null,
+                    status: 401
+                }, {
+                    method: 'post',
+                    auth: null,
+                    status: 401
+                }, {
+                    method: 'put',
+                    auth: null,
+                    status: 401
+                }, {
+                    method: 'delete',
+                    auth: null,
+                    status: 401
+                },
+                //non-existing account
+                {
+                    method: 'get',
+                    auth: true,
+                    status: 403,
+                    server: invalidServer
+                }, {
+                    method: 'post',
+                    auth: true,
+                    status: 403,
+                    server: invalidServer
+                }, {
+                    method: 'put',
+                    auth: true,
+                    status: 403,
+                    server: invalidServer
+                }, {
+                    method: 'delete',
+                    auth: true,
+                    status: 403,
+                    server: invalidServer
+                },
+                //server_url not provided or empty
+                {
+                    method: 'get',
+                    auth: true,
+                    status: 400,
+                    server: ''
+                }, {
+                    method: 'post',
+                    auth: true,
+                    status: 400,
+                    server: ''
+                }, {
+                    method: 'put',
+                    auth: true,
+                    status: 400,
+                    server: ''
+                }, {
+                    method: 'delete',
+                    auth: true,
+                    status: 400,
+                    server: ''
+                }
+            ];
 
-        v1Survey.map( function( obj ) {
-            obj.version = version;
-            obj.endpoint = '/survey';
-            return obj;
-        } ).forEach( testResponse );
+            v1Survey.map( function( obj ) {
+                obj.version = version;
+                obj.endpoint = '/survey';
+                return obj;
+            } ).forEach( testResponse );
+        } );
+
+        describe( '/survey endpoint offline-enabled and online-only responses (incompatible with v2)', function() {
+            // test online responses for /survey endpoint (differs in v2)
+            testResponse( {
+                version: version,
+                endpoint: '/survey',
+                method: 'post',
+                auth: true,
+                status: 200,
+                res: {
+                    property: 'url',
+                    expected: /\/::[A-z0-9]{4}/
+                },
+                offline: false
+            } );
+
+            // test offline responses for /survey endpoint (differs in v2)
+            testResponse( {
+                version: version,
+                endpoint: '/survey',
+                method: 'post',
+                auth: true,
+                status: 200,
+                res: {
+                    property: 'url',
+                    expected: /\/_#[A-z0-9]{4}/
+                },
+                offline: true
+            } );
+
+            // test offline responses for /survey/offline endpoint (differs in v2)
+            testResponse( {
+                version: version,
+                endpoint: '/survey/offline',
+                method: 'post',
+                auth: true,
+                status: 405,
+                offline: true
+            } );
+        } );
 
         // TODO: add some tests for other /survey/* and /surveys/number endpoints
 
-        v1Instance = [
-            // valid token
-            {
-                method: 'post',
-                auth: true,
-                instanceId: 'AAA',
-                status: 201
-            },
-            // already being edited
-            {
-                method: 'post',
-                auth: true,
-                instanceId: 'AAA',
-                status: 405
-            },
-            // test return url in response
-            {
-                method: 'post',
-                auth: true,
-                ret: 'http://enke.to',
-                status: 201,
-                res: {
-                    property: 'edit_url',
-                    expected: /.+\?.*returnUrl=http%3A%2F%2Fenke.to/
+        describe( '/instance endpoint responses', function() {
+            v1Instance = [
+                // valid token
+                {
+                    method: 'post',
+                    auth: true,
+                    instanceId: 'AAA',
+                    status: 201
+                },
+                // already being edited
+                {
+                    method: 'post',
+                    auth: true,
+                    instanceId: 'AAA',
+                    status: 405
+                },
+                // test return url in response
+                {
+                    method: 'post',
+                    auth: true,
+                    ret: 'http://enke.to',
+                    status: 201,
+                    res: {
+                        property: 'edit_url',
+                        expected: /.+\?.*returnUrl=http%3A%2F%2Fenke.to/
+                    }
+                },
+                // invalid parameters
+                {
+                    method: 'post',
+                    auth: true,
+                    id: '',
+                    status: 400
+                }, {
+                    method: 'post',
+                    auth: true,
+                    instance: '',
+                    status: 400
+                }, {
+                    method: 'post',
+                    auth: true,
+                    instanceId: '',
+                    status: 400
+                }, {
+                    method: 'post',
+                    auth: true,
+                    ret: '',
+                    status: 400
+                }, {
+                    method: 'post',
+                    auth: true,
+                    server: '',
+                    status: 400
+                },
+                // different methods, valid token
+                {
+                    method: 'get',
+                    auth: true,
+                    status: 405
+                }, {
+                    method: 'put',
+                    auth: true,
+                    status: 405
+                },
+                // removes instance from db
+                {
+                    method: 'delete',
+                    auth: true,
+                    status: 204
+                },
+                // no account 
+                {
+                    method: 'post',
+                    auth: true,
+                    status: 403,
+                    server: 'https://testserver.com/notexist'
                 }
-            },
-            // invalid parameters
-            {
-                method: 'post',
-                auth: true,
-                id: '',
-                status: 400
-            }, {
-                method: 'post',
-                auth: true,
-                instance: '',
-                status: 400
-            }, {
-                method: 'post',
-                auth: true,
-                instanceId: '',
-                status: 400
-            }, {
-                method: 'post',
-                auth: true,
-                ret: '',
-                status: 400
-            }, {
-                method: 'post',
-                auth: true,
-                server: '',
-                status: 400
-            },
-            // different methods, valid token
-            {
-                method: 'get',
-                auth: true,
-                status: 405
-            }, {
-                method: 'put',
-                auth: true,
-                status: 405
-            },
-            // removes instance from db
-            {
-                method: 'delete',
-                auth: true,
-                status: 204
-            },
-            // no account 
-            {
-                method: 'post',
-                auth: true,
-                status: 403,
-                server: 'https://testserver.com/notexist'
-            }
-        ];
+            ];
 
-        v1Instance.map( function( obj ) {
-            obj.version = version;
-            obj.endpoint = '/instance';
-            return obj;
-        } ).forEach( testResponse );
+            v1Instance.map( function( obj ) {
+                obj.version = version;
+                obj.endpoint = '/instance';
+                return obj;
+            } ).forEach( testResponse );
+        } );
     } );
 
 
     describe( 'v2', function() {
         var version = 2;
 
-        // make sure v2 is backwards-compatible with v1
-        v1Survey.map( function( obj ) {
-            obj.version = version;
-            return obj;
-        } ).forEach( testResponse );
+        describe( 'v1-compatible /survey endpoint responses', function() {
+            // make sure v2 is backwards-compatible with v1
+            v1Survey.map( function( obj ) {
+                obj.version = version;
+                return obj;
+            } ).forEach( testResponse );
+        } );
 
-        // make sure v2 is backwards-compatible with v1
-        v1Instance.map( function( obj ) {
-            obj.version = version;
-            if ( obj.instanceId === 'AAA' ) {
-                obj.instanceId = 'BBB';
-            }
-            return obj;
-        } ).forEach( testResponse );
+        describe( 'v1-compatible /instance endpoint responses', function() {
+            // make sure v2 is backwards-compatible with v1
+            v1Instance.map( function( obj ) {
+                obj.version = version;
+                if ( obj.instanceId === 'AAA' ) {
+                    obj.instanceId = 'BBB';
+                }
+                return obj;
+            } ).forEach( testResponse );
+        } );
 
         [
+            // TESTING THE OFFLINE/ONLINE VIEWS (not compatible with v1)
+            // the /survey endpoint always returns the online-only view
+            {
+                version: version,
+                endpoint: '/survey',
+                method: 'post',
+                auth: true,
+                status: 200,
+                res: {
+                    property: 'url',
+                    expected: /\/::[A-z0-9]{4}/
+                },
+                offline: false
+            }, {
+                version: version,
+                endpoint: '/survey',
+                method: 'post',
+                auth: true,
+                status: 200,
+                res: {
+                    property: 'url',
+                    expected: /\/::[A-z0-9]{4}/
+                },
+                offline: true
+            },
+            // the /survey/offline endpoint always returns the offline-capable view (if enabled)
+            {
+                version: version,
+                endpoint: '/survey/offline',
+                method: 'post',
+                auth: true,
+                status: 405,
+                offline: false
+            }, {
+                version: version,
+                endpoint: '/survey/offline',
+                method: 'post',
+                auth: true,
+                status: 200,
+                res: {
+                    property: 'offline_url',
+                    expected: /\/_#[A-z0-9]{4}/
+                },
+                offline: true
+            },
             // TESTING THE DEFAULTS[] PARAMETER
             // defaults are optional
             {
