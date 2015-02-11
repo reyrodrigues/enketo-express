fa2e21484b2c5640d1f8666aa69c8a400865598e
Martijn van de Rijdt <martijn@enketo.org>
2015-02-11 14:55:10 -0700

    functioning manifest creation

--- app/controllers/manifest-controller.js
+++ app/controllers/manifest-controller.js
@@ -1,9 +1,7 @@
 "use strict";
 
-var libxml = require( "libxmljs" ),
-    url = require( 'url' ),
-    path = require( 'path' ),
-    fs = require( 'fs' ),
+var manifest = require( '../models/manifest-model' ),
+    Q = require( 'q' ),
     express = require( 'express' ),
     router = express.Router(),
     debug = require( 'debug' )( 'manifest-controller' );
@@ -13,82 +11,30 @@ module.exports = function( app ) {
 };
 
 router
-    .get( '/_/manifest.appcache', manifest );
+    .get( '/_/manifest.appcache', function( req, res, next ) {
 
-function manifest( req, res, next ) {
-    res
-        .set( 'Content-Type', 'text/cache-manifest' )
-        .send( getManifestContent( req, res, next ) );
-}
+        getManifest( req, res )
+            .then( function( manifestContent ) {
+                res
+                    .set( 'Content-Type', 'text/cache-manifest' )
+                    .send( manifestContent );
+            } )
+            .catch( next );
+    } );
 
-function getManifestContent( req, res, next ) {
-    var value, doc,
-        resources = [];
+
+function getManifest( req, res ) {
+    var deferred = Q.defer();
 
     res.render( 'surveys/webform', {
         offline: true
     }, function( err, html ) {
-
         if ( err ) {
-            next( err );
+            deferred.reject( err );
+        } else {
+            deferred.resolve( manifest.get( html, req.i18n.lng() ) );
         }
-
-        doc = libxml.parseHtml( html );
-
-        // href attributes of link elements
-        doc.find( '//link[@href]' ).forEach( function( element ) {
-            resources.push( element.attr( 'href' ).value() );
-        } );
-
-        // additional themes
-        resources.forEach( function( resource ) {
-            var themeStyleSheet = /theme-([A-z]+)(\.print)?\.css$/;
-            if ( themeStyleSheet.test( resource ) ) {
-                var foundTheme = resource.match( themeStyleSheet )[ 1 ];
-                req.app.get( 'themes supported' ).forEach( function( theme ) {
-                    if ( foundTheme !== theme ) {
-                        resources.push( resource.replace( foundTheme, theme ) );
-                    }
-                } );
-            }
-        } );
-
-        // any resources inside css files
-        resources.forEach( function( resource ) {
-            var css = /^.+\.css$/;
-            if ( css.test( resource ) ) {
-                try {
-                    var content = fs.readFileSync( path.join( __dirname, '../../public', resource ), 'utf8' );
-                    var matches = content.match( /url[\s]?\([\s]?[\'\"]?([^\)\'\"]+)[\'\"]?[\s]?\)/g );
-                    debug( 'urls found', matches );
-                    // TODO add URLs
-                } catch ( e ) {}
-            }
-        } );
-
-        // src attributes
-        doc.find( '//*[@src]' ).forEach( function( element ) {
-            value = element.attr( 'src' ).value();
-            if ( _belongsInManifest( value ) ) {
-                resources.push( value );
-            }
-        } );
-
     } );
 
-    return 'CACHE MANIFEST\n' +
-        '# hash:' + Math.random() + '\n' +
-        '\n' +
-        'CACHE:\n' +
-        resources.join( '\n' ) + '\n' +
-        '\n' +
-        'FALLBACK:\n' +
-        '/ /offline\n' +
-        '\n' +
-        'NETWORK\n' +
-        '*\n';
-}
-
-function _belongsInManifest( resourceUrl ) {
-    return url.parse( resourceUrl ).protocol !== 'data:';
+    return deferred.promise;
 }
---
+++ app/models/manifest-model.js
@@ -0,0 +1,159 @@
+"use strict";
+
+var libxml = require( "libxmljs" ),
+    url = require( 'url' ),
+    path = require( 'path' ),
+    fs = require( 'fs' ),
+    utils = require( '../lib/utils' ),
+    debug = require( 'debug' )( 'manifest-model' );
+
+function getManifest( html, lang ) {
+    var hash,
+        doc = libxml.parseHtml( html ),
+        resources = [],
+        app = require( '../../config/express' ),
+        themesSupported = app.get( 'themes supported' ) || [];
+
+    // href attributes of link elements
+    resources = resources.concat( _getLinkHrefs( doc ) );
+
+    // additional themes
+    resources = resources.concat( _getAdditionalThemes( resources, themesSupported ) );
+
+    // translations
+    resources = resources.concat( _getTranslations( lang ) );
+
+    // any resources inside css files
+    resources = resources.concat( _getResourcesFromCss( resources ) );
+
+    // src attributes
+    resources = resources.concat( _getSrcAttributes( doc ) );
+
+    // remove non-existing files, empties, duplicates and non-http urls
+    resources = resources
+        .filter( _removeEmpties )
+        .filter( _removeDuplicates )
+        .filter( _removeNonHttpResources )
+        .filter( _removeNonExisting );
+
+    // calculate the hash to serve as the manifest version number
+    hash = _getHashFromResources( resources );
+
+    return 'CACHE MANIFEST\n' +
+        '# hash:' + hash + '\n' +
+        '\n' +
+        'CACHE:\n' +
+        resources.join( '\n' ) + '\n' +
+        '\n' +
+        'FALLBACK:\n' +
+        '/ /offline\n' +
+        '\n' +
+        'NETWORK:\n' +
+        '*\n';
+}
+
+function _getLinkHrefs( doc ) {
+    return doc.find( '//link[@href]' ).map( function( element ) {
+        return element.attr( 'href' ).value();
+    } );
+}
+
+function _getSrcAttributes( doc ) {
+    return doc.find( '//*[@src]' ).map( function( element ) {
+        return element.attr( 'src' ).value();
+    } );
+}
+
+function _getAdditionalThemes( resources, themes ) {
+    var urls = [];
+
+    resources.forEach( function( resource ) {
+        var themeStyleSheet = /theme-([A-z]+)(\.print)?\.css$/;
+        if ( themeStyleSheet.test( resource ) ) {
+            var foundTheme = resource.match( themeStyleSheet )[ 1 ];
+            themes.forEach( function( theme ) {
+                var themeUrl = resource.replace( foundTheme, theme );
+                urls.push( themeUrl );
+            } );
+        }
+    } );
+
+    return urls;
+}
+
+function _getTranslations( lang ) {
+    return ( lang ) ? '/locales/' + lang + '/translation.json' : null;
+}
+
+function _getResourcesFromCss( resources ) {
+    var content, matches,
+        urlReg = /url\(['|"]?([^\)'"]+)['|"]?\)/g,
+        cssReg = /^.+\.css$/,
+        urls = [];
+
+    resources.forEach( function( resource ) {
+        if ( cssReg.test( resource ) ) {
+            content = _getResourceContent( resource );
+            while ( ( matches = urlReg.exec( content ) ) !== null ) {
+                urls.push( matches[ 1 ] );
+            }
+        }
+    } );
+
+    return urls;
+}
+
+function _getResourceContent( resource ) {
+    var rel;
+    // in try catch in case css file is missing
+    try {
+        rel = ( resource.indexOf( '/locales/' ) === 0 ) ? '../../' : '../../public';
+        return fs.readFileSync( path.join( __dirname, rel, url.parse( resource ).pathname ), 'utf8' );
+    } catch ( e ) {
+        return '';
+    }
+}
+
+function _removeNonExisting( resource ) {
+    var rel = ( resource.indexOf( '/locales/' ) === 0 ) ? '../../' : '../../public',
+        resourcePath = path.join( __dirname, rel, url.parse( resource ).pathname ),
+        // TODO: in later versions of node.js, this should be replaced by: fs.accessSync(resourcePath, fs.R_OK)
+        exists = fs.existsSync( resourcePath );
+
+    if ( !exists ) {
+        debug( 'cannot find', resourcePath );
+    }
+    return exists;
+}
+
+function _removeEmpties( resource ) {
+    return !!resource;
+}
+
+function _removeDuplicates( resource, position, array ) {
+    return array.indexOf( resource ) == position;
+}
+
+function _removeNonHttpResources( resourceUrl ) {
+    var parsedUrl = url.parse( resourceUrl );
+    return parsedUrl.path && parsedUrl.protocol !== 'data:';
+}
+
+function _getHashFromResources( resources ) {
+    var content,
+        hash = '';
+
+    resources.forEach( function( resource ) {
+        try {
+            content = _getResourceContent( resource );
+            hash += utils.md5( content );
+        } catch ( e ) {}
+    } );
+
+    // shorten hash
+    return utils.md5( hash );
+}
+
+module.exports = {
+    get: getManifest
+};
